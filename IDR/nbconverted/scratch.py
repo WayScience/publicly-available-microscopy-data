4317887b469b50ab1a4ec08cfbf310aaa8d27abe
phicks22 <parkerhicks99@gmail.com>
2022-09-02 12:32:22 -0600

    Removed duplicate nbconverted directory

--- IDR/nbconverted/scratch.py
+++
@@ -1,9 +0,0 @@
-# SKIP THIS STEP FOR FULL DATASET ACCESS
-# Get a random image id:
-# rand_image = None
-# while rand_image is None:
-#     rand_row = rowlabels.index(random.choice(rowlabels))
-#     rand_col = collabels.index(random.choice(collabels))
-#     cell = grid["grid"][rand_row][rand_col]
-#     if cell is not None:
-#         rand_image = cell['id']
