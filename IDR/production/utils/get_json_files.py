f0093b26652b5441b95ca6d1548eabf76f64dcf3
phicks22 <parkerhicks99@gmail.com>
2022-09-21 14:01:32 -0600

    Added json urls

--- IDR/production/utils/get_json_files.py
+++ IDR/production/utils/get_json_files.py
@@ -2,3 +2,6 @@ import json
 import requests
 
 
+PLATES_IN_SCREEN_URL = f"https://idr.openmicroscopy.org/webclient/api/plates/?id={screen_id}"
+WELLS_IN_PLATES_URL = f"https://idr.openmicroscopy.org/webgateway/plate/{plate}/"
+MAP_URL = f"https://idr.openmicroscopy.org/webclient/api/annotations/?type=map&well={wellID}"
