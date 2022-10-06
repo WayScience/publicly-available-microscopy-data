d70db729df0f80f8575b8549406f80fb900b1254
phicks22 <parkerhicks99@gmail.com>
2022-10-05 18:08:29 -0600

    Added extraction pipeline

--- IDR/production/metadata_extraction/download_jsons/process_json_metadata.py
+++ IDR/production/metadata_extraction/download_jsons/process_json_metadata.py
@@ -1,11 +1,29 @@
+from email.mime import image
 import pathlib
 import json
+import time
+import os
 import pandas as pd
 from utils.io import walk
+from utils.clean_channels import clean_channel
+import multiprocessing
 
 
-def pull_metadata_from_json(
-    json_file_paths,
+def flatten_list(list_):
+    return [item for sublist in list_ for item in sublist]
+
+def nested_list_to_dict(nested_list):
+    return {k[0]: k[1:][0] for k in nested_list}
+
+def iterate_through_values(annotations):
+    values_list = [sub_dict['values'] for sub_dict in annotations]
+    flattened_list = flatten_list(values_list)
+    values_dict = nested_list_to_dict(flattened_list)
+    return values_dict
+
+
+def pull_json_well_metadata(
+    well_metadata_file,
     image_attributes,
 ):
     """Pull metadata from downloaded json metadata files
@@ -27,166 +45,106 @@ def pull_metadata_from_json(
     Returns
     -------
     metadata: list
-        List of metadata values for attribute in image_attributes
+        List of metadata values for attribute in image_attributes per well
     """
-    json_keys = {
-        "Channels": "omero/bulk_annotations",
-        "Organism": "mapr/organism",
-        "Cell Line": "mapr/cell_line",
-        "Oraganism Part": "omero/bulk_annotations",
-        "Strain": "omero/bulk_annotations",
-        "Gene Identifier": "mapr/gene",
-        "Phenotype Identifier": "mapr/phenotype",
-    }
+    plate_id = str(well_metadata_file).split("/")[-2]
+    well_id = str(well_metadata_file).split("/")[-1].removesuffix(".json")
 
-    for plate in study_plates:
-        wellIDs = list()
-        plate_name = study_plates[plate]
+    with well_metadata_file.open() as data_file:
+        json_dict = json.load(data_file)
 
-        # Access .json file for the plate ID. Contains image ID (id) numbers
-        # for replicate images per plate.
-        WELLS_IMAGES_URL = f"https://idr.openmicroscopy.org/webgateway/plate/{plate}/"
-        grid = session.get(WELLS_IMAGES_URL).json()
+    well_results_dict = dict()
+    annotations = json_dict["annotations"]
+    annotation_values = iterate_through_values(annotations=annotations)
+    for image_attribute in image_attributes:
+        if image_attribute in annotation_values:
+            if image_attribute == "Channels":
+                well_results_dict[image_attribute] = clean_channel(annotation_values[image_attribute])
+            else:
+                well_results_dict[image_attribute] = annotation_values[image_attribute]
+        else:
+            well_results_dict[image_attribute] = "Not listed"
 
-        try:
-            pixel_size_x = grid["image_sizes"][0]["x"]
-            pixel_size_y = grid["image_sizes"][0]["y"]
-
-            # Get well ids
-            excluded_keys = ["collabels", "rowlabels", "image_sizes"]
-            for key in excluded_keys:
-                well_JSON.pop(key, None)
-            for plate_row in range(len(well_JSON["grid"])):
-                for well in well_JSON["grid"][row]:
-                    # Append IDs to iterable lists
-                    wellIDs.append(well["wellId"])
-
-        except (ValueError, KeyError):
-            plate_results.append(
-                [
-                    screen_id,
-                    plate,
-                    plate_name,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                    None,
-                ]
-            )
-            continue
-
-        ANNOTATIONS_URL = "openmicroscopy.org/"
-
-        # Get image details from each image id for each plate
-        for wellID in wellIDs:
-            MAP_URL = f"https://idr.openmicroscopy.org/webclient/api/annotations/?type=map&image={id}"
-            annotations = session.get(MAP_URL).json()["annotations"]
-
-            for image_attribute in image_attributes:
-                ATTRIBUTE_URL = ANNOTATIONS_URL + json_keys[image_attribute]
-
-                try:
-                    attribute_index = [x["ns"] for x in annotations].index(
-                        ATTRIBUTE_URL
-                    )
-                    image_attribute_value = {
-                        x[0]: x[1] for x in annotations[attribute_index]["values"]
-                    }[image_attribute]
-
-                except (ValueError, KeyError):
-                    image_attribute_value = "Not Listed"
-
-            # Build results
-            plate_results.append(
-                [
-                    screen_id,
-                    study_name,
-                    plate,
-                    plate_name,
-                    wellID,
-                    imaging_method,
-                    sample,
-                    organism,
-                    organism_part,
-                    cell_line,
-                    strain,
-                    gene_identifier,
-                    phenotype_identifier,
-                    stains_targets,
-                    pixel_size_x,
-                    pixel_size_y,
-                ]
-            )
+    well_results_dict["plate_id"] = plate_id
+    well_results_dict["well_id"] = well_id
+    
+    return well_results_dict
 
 
 
-pull_metadata_from_json(image_attributes=["Channels",
-        "Organism",
-        "Cell Line",
-        "Oraganism Part",
-        "Strain",
-        "Gene Identifier"])
-
-
-def collect_metadata(idr_name, values_list):
+def collect_metadata(metadata_tuple):
     """ """
     data_dir = pathlib.Path("IDR/data/metadata")
     if data_dir.exists() == False:
-        os.mkdir(data_dir)
+        pathlib.mkdir(data_dir)
 
-    screen_id = values_list[0]
-    imaging_method = values_list[1]
-    sample = values_list[2]
-    split_name = idr_name.split("/")
-    study_name = split_name[0]
-    screen_name = split_name[1]
+    screen_id = metadata_tuple[0]
+    idr_name = metadata_tuple[1]
+    imaging_method = metadata_tuple[2]
+    sample = metadata_tuple[3]
+    split_idr_name = idr_name.split("/")
+    study_name = split_idr_name[0]
+    screen_name = split_idr_name[1]
 
-    # Make directories
+    # Make metadata subdirectories
     study_dir = pathlib.Path(data_dir, study_name)
     screen_dir = pathlib.Path(study_dir, screen_name)
-    if study_dir.exists() == False:
-        os.mkdir(study_dir)
-    elif screen_dir.exists() == False:
-        os.mkdir(screen_dir)
-    else:
-        pass
+    pathlib.Path.mkdir(study_dir, exist_ok=True)
+    pathlib.Path.mkdir(screen_dir, exist_ok=True)
+
+    # Create iterable list of json metadata files
+    json_metadata_screen_dir = pathlib.Path("IDR/data/json_metadata")
+    json_metadata_files = list(walk(json_metadata_screen_dir))
+
+    image_attributes=["Channels",
+                "Organism",
+                "Cell Line",
+                "Oraganism Part",
+                "Strain",
+                "Gene Identifier"]
 
     # Collect data
-    plate_results_df = describe_screen(
-        screen_id=screen_id,
-        sample=sample,
-        imaging_method=imaging_method,
-        study_name=study_name,
-    )
+    screen_results_dict = {image_attribute: list() for image_attribute in image_attributes}
+    for well_json_metadata in json_metadata_files:
+        well_results_list = pull_json_well_metadata(well_metadata_file=well_json_metadata, image_attributes=image_attributes)
+        for image_attribute in well_results_list.keys():
+            screen_results_dict[image_attribute].append(well_results_list[image_attribute])
 
+    screen_results_df = pd.DataFrame.from_dict(screen_results_dict)
+    screen_results_df['Imaging Method'] = pd.Series([imaging_method for x in range(len(screen_results_df.index))])
+    screen_results_df['Sample'] = pd.Series([sample for x in range(len(screen_results_df.index))])
+        
     # Save data per IDR accession name
     output_file = pathlib.Path(
         screen_dir, f"{study_name}_{screen_name}_{screen_id}.parquet.gzip"
     )
-    plate_results_df.to_parquet(output_file, compression="gzip")
+    screen_results_df.to_parquet(output_file, compression="gzip")
     
 
 if __name__ == "__main__":
 
-    # Load IDR IDs and screen details
+    # Load screen details
     data_dir = pathlib.Path("IDR/data")
-    id_file = pathlib.Path(data_dir, "idr_screen_ids.parquet")
     screen_details_file = pathlib.Path(data_dir, "screen_details.parquet")
 
-    # Load IDR IDs and screen details as pandas df
-    id_df = pd.read_parquet(id_file)
-    screen_details_df = pd.read_parquet(screen_details_file)
+    # Load pertinant columns of screen details as pandas df
+    screen_details_df = pd.read_parquet(screen_details_file)[["id", "idr_name", "Imaging Method", "Sample"]]
 
-    # Create iterable list of json metadata files
-    json_metadata_dir = pathlib.Path("IDR/data/json_metadata")
-    json_metadata_files = list(walk(json_metadata_dir))
+    # Create iterable list for multiprocessing
+    study_metadata = list(screen_details_df.itertuples(index=False, name=None))
 
+    # Set up Pool object
+    multiprocessing_iterable = list()
+    available_cores = len(os.sched_getaffinity(0))
+    pool = multiprocessing.Pool(processes=available_cores)
+
+    # Begin metadata collection
+    start = time.time()
+    pool.starmap(func=collect_metadata, iterable=study_metadata)
+
+    # Close multiprocess pool
+    pool.close()
+    pool.join()
+    print(f"\\nMetadata collected. Running cost is {(time.time()-start)/60:.1f} min.")
 
     print("Extraction method under development")
