c06d1f3106941f998635b569c64eed237c280869
phicks22 <parkerhicks99@gmail.com>
2022-10-12 11:12:54 -0600

    Added comments

--- IDR/production/2.visualize_stats.py
+++ IDR/production/2.visualize_stats.py
@@ -17,25 +17,28 @@ if __name__ == "__main__":
 
     dodge_text = position_dodge(width=0.9)
 
-    attributes_to_round = ["H", "J", "NME", "E", "GC"]
-    for attribute in attributes_to_round:
-        databank_stats[attribute] = databank_stats[attribute].apply(
+    # Round to 3 sigfigs if not 0
+    stats_to_round = ["H", "J", "NME", "E", "GC"]
+    for stat in stats_to_round:
+        databank_stats[stat] = databank_stats[stat].apply(
             lambda x: round(x, sigfigs=3) if x > 0 else x
         )
-        study_stats[attribute] = study_stats[attribute].apply(
+        study_stats[stat] = study_stats[stat].apply(
             lambda x: round(x, sigfigs=3) if x > 0 else x
         )
 
+    # Set output images directory
     study_imgs_dir = pathlib.Path("IDR/data/statistics/imgs/study_stat_imgs")
     databank_imgs_dir = pathlib.Path("IDR/data/statistics/imgs/databank_stat_imgs")
 
-    attributes_to_graph = ["H", "J", "NME", "E", "GC", "S"]
-    for attribute in attributes_to_graph:
+    # Plot stats for databank and individual study stats
+    stats_to_graph = ["H", "J", "NME", "E", "GC", "S"]
+    for stat in stats_to_graph:
         databank_plot = (
-            ggplot(data=databank_stats, mapping=aes(x="Attribute", y=attribute))
+            ggplot(data=databank_stats, mapping=aes(x="Attribute", y=stat))
             + geom_col(na_rm=True, stat="identity", position="identity")
             + geom_text(
-                aes(label=attribute),
+                aes(label=stat),
                 position=dodge_text,
                 color="gray",
                 size=8,
@@ -45,14 +48,14 @@ if __name__ == "__main__":
         )
 
         studies_plot = (
-            ggplot(data=study_stats, mapping=aes(x="Attribute", y=attribute))
+            ggplot(data=study_stats, mapping=aes(x="Attribute", y=stat))
             + geom_jitter(na_rm=True, stat="identity", position="jitter")
             + theme(axis_text_x=element_text(rotation=90))
-            + ylim(0, max(study_stats[attribute]))
+            + ylim(0, max(study_stats[stat]))
         )
 
-        databank_output_file = pathlib.Path(databank_imgs_dir, f"{attribute}.png")
+        databank_output_file = pathlib.Path(databank_imgs_dir, f"{stat}.png")
         databank_plot.save(databank_output_file)
 
-        studies_output_file = pathlib.Path(study_imgs_dir, f"{attribute}.png")
+        studies_output_file = pathlib.Path(study_imgs_dir, f"{stat}.png")
         studies_plot.save(studies_output_file)
