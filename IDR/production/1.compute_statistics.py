c16d8178666d61374fb5c6d6080c8250b83bc205
phicks22 <parkerhicks99@gmail.com>
2022-10-12 15:21:23 -0600

    Added unique entries and counts to df

--- IDR/production/1.compute_statistics.py
+++ IDR/production/1.compute_statistics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pathlib, sys
+from tqdm import tqdm
 from utils.statistics import collect_databank_stats, collect_study_stats
 
 # Define path to extraction_utils directory
@@ -8,12 +9,39 @@ sys.path.append(parent_dir)
 from metadata_extraction.extraction_utils.io import walk
 
 
+def nested_dict_pairs_iterator(elements_and_counts_dict):
+    """Collects all combinations of tuples for a nested dictionary
+
+    Parameters
+    ----------
+    elements_and_counts_dict: dict
+        Nested dictionary of all combinations of all unique entries of a metadata file
+        E.g. {study_name: {screen_id: {image_attribute: {unique_element: count}}}}
+    
+    Returns
+    -------
+    All possible combinations of tuples for a nested dictionary
+        E.g. (study_name1, screen_id1, image_attribute1, unique_element1, count1)
+    """
+    # Iterate over all key-value pairs of dict argument
+    for key, value in elements_and_counts_dict.items():
+        # Check if value is of dict type
+        if isinstance(value, dict):
+            # If value is dict then iterate over all its values
+            for pair in  nested_dict_pairs_iterator(value):
+                yield (key, *pair)
+        else:
+            # If value is not dict type then yield the value
+            yield (key, value)
+
+
 if __name__ == "__main__":
     # Define study metadata directory
     studies_metadata_dir = pathlib.Path("IDR/data/metadata")
 
     # Collect metadata file paths
     metadata_files = list(walk(studies_metadata_dir))
+    metadata_files = metadata_files[0:5]
 
     # Make directories
     stats_dir = pathlib.Path("IDR/data/statistics")
@@ -24,6 +52,7 @@ if __name__ == "__main__":
     unique_elements_and_counts = dict()
     for metadata_path in metadata_files:
         study_name = ((str(metadata_path).split("/")[-1]).split(".")[0]).split("_")[0]
+        screen_id = ((str(metadata_path).split("/")[-1]).split(".")[0]).split("_")[-1]
         _, attribute_elements = collect_study_stats(
             metadata_path,
             individual_study_stats,
@@ -32,23 +61,22 @@ if __name__ == "__main__":
                 "study_name",
                 "plate_name",
                 "plate_id",
-                "sample",
+                "well_id"
             ],
             study_name=study_name,
+            screen_id = screen_id
         )
 
         unique_elements_and_counts[study_name] = attribute_elements
 
     # Convert unique_elements_and_counts dict to Pandas.DataFrame
-    unique_elements_and_counts_df = pd.DataFrame.from_dict(
-        {
-            (study, elements_counts): unique_elements_and_counts[study][elements_counts]
-            for study in unique_elements_and_counts.keys()
-            for elements_counts in unique_elements_and_counts[study].keys()
-        },
-        orient="index",
-    )
+    unique_elements_and_counts_list = list(nested_dict_pairs_iterator(unique_elements_and_counts))
 
+    # Generate output dataframes
+    unique_elements_and_counts_df = pd.DataFrame(
+        data=unique_elements_and_counts_list, 
+        columns=["Study", "Screen", "Attribute", "Element", "n"],
+    )
     stat_results_df = pd.DataFrame(
         data=individual_study_stats,
         columns=["Study_Name", "Attribute", "S", "H", "NME", "J", "E", "GC"],
