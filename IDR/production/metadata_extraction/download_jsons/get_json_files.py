952cb17d61b011d986c952f3d6ba0dd89443e67a
phicks22 <parkerhicks99@gmail.com>
2022-10-28 13:43:41 -0600

    More descriptive variable names

--- IDR/production/metadata_extraction/download_jsons/get_json_files.py
+++ IDR/production/metadata_extraction/download_jsons/get_json_files.py
@@ -1,8 +1,10 @@
 import json
-import requests
+import os
 import pathlib
-from tqdm import tqdm
+
 import pandas as pd
+import requests
+from tqdm import tqdm
 
 if __name__ == "__main__":
     # Initialize session
@@ -20,6 +22,12 @@ if __name__ == "__main__":
     # Load idr screen ids
     idr_ids_file = pathlib.Path("IDR/data/idr_ids.tsv")
     screen_ids = pd.read_csv(idr_ids_file, sep="\t").id.values.tolist()
+
+    json_metadata_dir = pathlib.Path("IDR/data/json_metadata")
+    available_screens = os.listdir(json_metadata_dir)
+    available_screens = [int(screen_id) for screen_id in available_screens]
+
+    screen_ids = [id_ for id_ in screen_ids if id_ not in available_screens]
 
     # Get plate ids
     for screen_id in tqdm(screen_ids):
@@ -64,5 +72,5 @@ if __name__ == "__main__":
 
                 # Download the json files
                 output_file = pathlib.Path(output_dir, f"{wellID}.json")
-                with open(output_file, "w", encoding="utf-8") as f:
-                    json.dump(well_metadata, f, ensure_ascii=False, indent=4)
+                with open(output_file, "w", encoding="utf-8") as file:
+                    json.dump(well_metadata, file, ensure_ascii=False, indent=4)
