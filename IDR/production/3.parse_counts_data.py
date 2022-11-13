17ef793f583110b60063a3953cd2a307fb424d96
phicks22 <parkerhicks99@gmail.com>
2022-11-12 17:02:58 -0700

    Sorting counts for all atts

--- IDR/production/3.parse_counts_data.py
+++ IDR/production/3.parse_counts_data.py
@@ -21,8 +21,25 @@ def sort_counts(attribute_: str, element_counts_df: pd.DataFrame):
 
 if __name__ == "__main__":
     # Read counts parquet file as pd.DataFrame
-    counts_data = pathlib.Path("../data/statistics/unique_elements_and_counts.parquet")
+    counts_data = pathlib.Path("IDR/data/statistics/unique_elements_and_counts.parquet")
     counts_df = pd.read_parquet(counts_data)[["Study", "Attribute", "Element", "Count"]]
 
     # Standardize elements to lowercase 
     counts_df["Element"] = counts_df["Element"].str.lower()
+
+    # Define attributes and file naming schema 
+    attributes = {"Phenotype": "Phenotype", "Gene Symbol": "Gene_Symbol", "Compound Name": "Compound", "Cell Line": "Cell_Line", "siRNA Identifier": "siRNA"}
+    
+    # Iterate through attributes to sort
+    for attribute in attributes:
+        final_df = sort_counts(attribute_=attribute, element_counts_df=counts_df)
+
+        # Account for alternative phenotype names
+        if attribute == "Phenotype":
+            alt_df = sort_counts(attribute_="Phenotype Term Name", element_counts_df=counts_df)
+            final_df = pd.concat([final_df, alt_df]).reset_index()
+        
+        # Save attribute counts as individual csv files
+        out_path = pathlib.Path("IDR/data/Counts")
+        output_file = pathlib.Path(out_path, f"{attributes[attribute]}_Counts.csv")
+        final_df.to_csv(output_file)
